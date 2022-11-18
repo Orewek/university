4963ad14eef4354c132bf40d532626a798ed5f6e
Orewek <danilacharykov@gmail.com>
2022-11-18 15:07:52 +0300

    mypy for check

--- massive_laba/Unit_tests/some_checks.py
+++ massive_laba/Unit_tests/some_checks.py
@@ -1,7 +1,10 @@
-def check_int(int_variable: any) -> int:
+from typing import Any
+
+
+def check_int(int_variable: Any) -> int:
     while int_variable.isdigit() is False:
-            print('You can write only digits')
-            int_variable = input()
+        print('You can write only digits')
+        int_variable = input()
 
     return int_variable
 
