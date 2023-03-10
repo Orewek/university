8035fc1e004d63922bfa2faa65cd8ac61bda504e
Orewek <charykov.d@sch2009.net>
2023-03-10 08:30:41 +0300

    need to make flake and clang

--- second_term/second_prog/polish_notation.py
+++ second_term/second_prog/polish_notation.py
@@ -4,19 +4,18 @@ from make_postfix import infix_to_postfix
 
 
 def remove_last_line(lines_amount: int, file_name: str, delete: int) -> None:
-    i = 1
     with open(r".\txts\temp.txt", "r") as f:
         with open(rf".\txts\{file_name}.txt", "w") as s:
             for line in f:
                 s.write(line)
-
+    os.rename(r)
     os.remove(r"txts\temp.txt")
 
 
 def main() -> None:
     parser_infix = ctypes.WinDLL(r".\dll\parser_infix.dll")
     print("PARSER_INFIX WAS SUCCESSFULLY LOADED")
-    
+
     our_string = str(input())
     our_string_utf = our_string.encode('utf-8')
 
@@ -29,13 +28,10 @@ def main() -> None:
 
     postfix_utf = postfix.encode('utf-8')
     parser_infix.parse.arguments = [ctypes.c_char_p]
- 
+
     new_lines_amount = parser_infix.parse(postfix_utf)
     remove_last_line(new_lines_amount, "postfix", 1)
     print(f'\n {new_lines_amount}')
-
-
-
 
 
 if __name__ == '__main__':
