52fa2d333f1363634518d31bf3604428edffbb83
Orewek <danilacharykov@gmail.com>
2023-04-11 13:26:12 +0300

    user interface and additional tests for input variables

--- second_term/third_prog/build.py
+++ second_term/third_prog/build.py
@@ -2,41 +2,55 @@ import csv
 
 
 def cut(length_prices: list) -> int:
-    copy_prices = length_prices[:]
+    prices = length_prices[:]
     for i in range(1, len(length_prices)):
         for j in range((i // 2) + 1):
-            copy_prices[i] = max(copy_prices[i], copy_prices[i - j] + copy_prices[j])
-    return copy_prices[len(length_prices) - 1]
+            prices[i] = max(prices[i], prices[i - j] + prices[j])
+    return prices[len(length_prices) - 1]
 
 
 def input_prices(length_prices: list) -> list:
     print('Write numbers one-by-one, write "exit" to quit')
-    price = None
-    while price != "exit":
+    price = ''
+    meters = 1
+    while price.lower() != "exit":
+        print(f'Write a price for {meters} meters')
         price = input()
         if price.isdigit() is True:
+            meters += 1
             length_prices.append(int(price))
+        elif price.lower() != "exit":
+            print('You can write only numbers as a price')
 
-    print(f'Prices were succsessfully added\n Modidied list: {length_prices}')
+    print(f'Prices were succsessfully added\n'
+          f'Modidied list: {length_prices[1:]}')
+    return length_prices
+
+
+def read_info_from_file(length_prices: list) -> list:
+    with open('prices.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        lines = 0
+        for row in csv_reader:
+            if lines > 0 and row != []:
+                print(f'${row[0]} for {lines} meters')
+                length_prices.append(int(row[0]))
+            lines += 1
+
     return length_prices
 
 
 def main() -> None:
     print('Do you want to write prices manually or from file?')
     print('1: file\n2: manually\n')
-    action = int(input())
-    if action == 1:
-        length_prices = []
-        with open('prices.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
 
-            lines = 0
-            for row in csv_reader:
-                if lines > 0 and row != []:
-                    print(f'${row[0]} for {lines - 1} meters')
-                    length_prices.append(int(row[0]))
-                lines += 1
+    action = input()
+    while action.isdigit() is False or (int(action) != 1 and int(action) != 2):
+        print('You can write only 1 or 2')
+        action = input()
 
+    if int(action) == 1:
+        length_prices = read_info_from_file([0])
     else:
         length_prices = input_prices([0])
 
@@ -51,7 +65,7 @@ def main() -> None:
 
     optimal_price = cut(length_prices)
 
-    print(f'Optimal price for {steel_length} meters of steel is ${optimal_price}')
+    print(f'Optimal price for {steel_length} meters is ${optimal_price}')
 
 
 if __name__ == '__main__':
