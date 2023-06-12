8ae0f5e56476f7ac8a1dd0c797302b3c8e23cc12
Orewek <charykov.d@sch2009.net>
2023-06-12 22:04:11 +0300

    get real

--- second_term/matlab_video/csv_2/make.py
+++
@@ -1,7 +0,0 @@
-mas = []
-with open('FilteredDetections copy.csv') as f:
-    mas = f.readlines()
-
-with open('new_file.xlsx', 'w') as ff:
-    for i in range(len(mas)):
-        ff.write(f'{mas[i][:55]}\n')
