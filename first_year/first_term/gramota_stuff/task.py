c2a62bd9d2cadf5a253b5be11a376c2b6a6d7871
Orewek <charykov.d@sch2009.net>
2024-04-13 12:36:05 +0300

    new linter stuff and variables to reduce lines length

--- first_year/first_term/gramota_stuff/task.py
+++ first_year/first_term/gramota_stuff/task.py
@@ -2,47 +2,50 @@ def normal_view(integer: str, frac_part: str) -> str:
     """
     number to a normal view
     """
+    int_len_frac_part: int = len(str(int(frac_part)))
+    len_frac_part: int = len(frac_part)
+    str_len_frac_part: str = str(len_frac_part)
+    con: str = ''
     if abs(int(integer)) >= 1:
         if not (integer.startswith('-')):
             return f'{integer[0]}.{integer[1:]}{frac_part} * 10^{str(len(integer[1:]))}'
         return f'{integer[0:2]}.{integer[2:]}{frac_part} * 10^{str(len(integer[1:]) - 1)}'
-    else:
-        if (integer.startswith('-')):
-            negative = '-'
-        else:
-            negative = ''
-        if len(str(int(frac_part))) != 1:
-            return f'{negative}{str(int(frac_part))[0]}, {str(int(frac_part))[1:]} * 10^(-{str(len(frac_part) - len((str(int(frac_part)))) + 1)})'
-        else:
-            return f'{negative}{str(int(frac_part))} * 10^(-{str(len(frac_part) - len(str(int(frac_part))))})'
+
+    if (integer.startswith('-')):
+        con: str = '-'
+    if len(str(int(frac_part))) != 1:
+        return f'{con}{str_len_frac_part[0]}, {str_len_frac_part[1:]} * 10^(-{str(len_frac_part - int_len_frac_part + 1)})'
+    return f'{con}{str_len_frac_part} * 10^(-{str(len_frac_part - int_len_frac_part)})'
 
 
 def non_normal_view(integer: str, frac_part: str) -> str:
+    int_len_frac_part: int = len(str(int(frac_part)))
+    len_frac_part: int = len(frac_part)
     if abs(int(integer)) >= 1:
         if not (integer.startswith('-')):
             return f'0.{integer}{frac_part} * 10^{str(len(integer))}'
         return f'-0.{integer[1:]}{frac_part} * 10^{str(len(integer) - 1)}'
+
+    if len(frac_part) != int_len_frac_part:
+        frac_part: str = frac_part[len_frac_part - int_len_frac_part:]
+    if integer == '0':
+        return f'0.{frac_part} * 10^{str(len_frac_part - int_len_frac_part)}'
     else:
-        if len(frac_part) != len(str(int(frac_part))):
-                frac_part = frac_part[len(frac_part) - len(str(int(frac_part))):]
-        if integer == '0':
-            return f'0.{frac_part} * 10^{str(len(frac_part) - len(str(int(frac_part))))}'
-        else:
-            return f'-0.{frac_part} * 10^{str(len(frac_part) - len(str(int(frac_part))))}'
+        return f'-0.{frac_part} * 10^{str(len_frac_part - int_len_frac_part)}'
 
 
 def to_bin_frac(frac_part: str) -> str:
     """
     to bin frac part
     """
-    amount_of_digits = 20
-    bin_frac = ''
+    amount_of_digits: int = 20
+    bin_frac: str = ''
     if len(str(int(frac_part))) == len(frac_part):
-        frac_part = int(frac_part)
+        frac_part: int = int(frac_part)
         frac_part /= 10**(len(str(frac_part)))
     else:
         zeroes_beginning_frac = len(frac_part) - len(str(int(frac_part)))
-        frac_part = int(frac_part) / 10**(zeroes_beginning_frac + 1)
+        frac_part: int = int(frac_part) / 10**(zeroes_beginning_frac + 1)
 
     while amount_of_digits:
         frac_part *= 2
@@ -61,25 +64,25 @@ def to_bin_frac(frac_part: str) -> str:
 def main() -> None:
     try:
         print('Input a number % 1 != 0\n')
-        number = input()
+        number: str = input()
         # number = '95821.99123'
         integer, frac_part = str(number).split('.')
 
         if integer.startswith('-'):
-            negative = '-'
+            con: str = '-'
         else:
-            negative = ''
+            con: str = ''
 
         if int(integer) != 0:
-            number = negative + str(bin(int(integer))[2:] + '.' + to_bin_frac(str(frac_part)))
+            number: str = con + str(bin(int(integer))[2:] + '.' + to_bin_frac(str(frac_part)))
         else:
-            number = f'{negative}0.{to_bin_frac(frac_part)}'
+            number: str = f'{con}0.{to_bin_frac(frac_part)}'
 
         print(f'normalized view: {normal_view(integer, frac_part)}')
         print(f'non-normalized view: {non_normal_view(integer, frac_part)}')
 
         # bin to 32bits
-        number_3e_32bit_bin = ''
+        number_3e_32bit_bin: str = ''
         if number.startswith('-'):
             number_3e_32bit_bin += '1'
         else:
@@ -87,16 +90,16 @@ def main() -> None:
 
         number = number.split('.')
         # to find out pow(10, exp)
-        exp = len(number[0]) - 1
+        exp: int = len(number[0]) - 1
         # pow of exponent
         number_3e_32bit_bin += '0' * (8 - len(bin(127 + exp)[2:])) + bin(127 + exp)[2:]
         # remainder of mantissa
         number_3e_32bit_bin += (str(number[0]) + str(number[1]))[1:]
         # adding zeros to the end
-        number_3e_32bit_bin = number_3e_32bit_bin.ljust(32, '0')
+        number_3e_32bit_bin: str = number_3e_32bit_bin.ljust(32, '0')
         print(f'number into IEEE 32bit: {number_3e_32bit_bin}')
 
-        number_3e_32bit_hex = ''.join([hex(int(number_3e_32bit_bin[i:i + 4], 2))[2:] for i in range(0, 32, 4)])
+        number_3e_32bit_hex: str = ''.join([hex(int(number_3e_32bit_bin[i:i + 4], 2))[2:] for i in range(0, 32, 4)])
         print(f'hex in 32bits: {number_3e_32bit_hex}')
     except:
         print('Error! number is an integer or fractional part is tooo small')
@@ -105,9 +108,9 @@ def main() -> None:
 if __name__ == '__main__':
     main()
 
-    approved = ['y', 'yes', '1']
-    additional_check = input()
+    approved: list = ['y', 'yes', '1']
+    additional_check: str = input()
     while additional_check.lower():
         main()
-        additional_check = input()
+        additional_check: str = input()
         print(f'One more?\n {approved}')
